c20241d69970a9678636ed8399fd4246dde8d855
smiyaguchi <smiyaguchi96@gmail.com>
2018-03-26 13:23:05 +0000

    Add .gitconfig and fix install script

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 DOTFILES_DIR=~/dotfiles
 
 mkdir ~/dotfiles
-git clone https://github.com/smiyaguchi/dotfiles.git $DOTFILES_DIR
 
+git clone https://github.com/smiyaguchi/dotfiles.git $DOTFILES_DIR
 git clone http://github.com/VundleVim/Vundle.Vim.git $DOTFILES_DIR/.vim/plugin/vundle.vim
 
 ln -s $DOTFILES_DIR/.vimrc .vimrc
+ln -s $DOTFILES_DIR/.gitconfig .gitconfig
