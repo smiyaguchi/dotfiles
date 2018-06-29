3029968b48477152de2ed01afeacce2679158091
smiyaguchi <smiyaguchi96@gmail.com>
2018-06-29 14:16:20 +0000

    Fix create link

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#...
+
+DOTPATH=~/dotfiles
+
+cd "$DOPATH"
+if [ $? -ne 0 ]; then
+    die "not found: $DOPATH"
+fi
 
 for f in .??*
 do
-	[ "$f" = ".git" ] && continue
-	ln -snfv "$f" "$HOME"/"$f"
+    [ "$f" = ".git" ] && continue
+    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
 
 git clone http://github.com/VundleVim/Vundle.Vim.git ./.vim/plugin/vundle.vim
